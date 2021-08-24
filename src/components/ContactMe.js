6858f18c0881adc945dab5070674ceddc1b1c389
unknown <mindquaker20@gmail.com>
2021-08-24 19:27:30 -0400

    reverting commit 9bbfb7d17a987b5b2f74a6d98bf35fe777ca0b96

---
+++ src/components/ContactMe.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const ContactMe = () => {
+  return <div>Contact Me</div>;
+};
+
+export default ContactMe;
---
+++ src/components/Skills.js
@@ -0,0 +1,91 @@
+import React from "react";
+
+export const Skills = () => {
+  return (
+    <div className=' h-screen w-screen'>
+      <div className='relative m-auto h-5/6 w-1/2'>
+        <div className='flex flex-row'>
+          <div className='mr-44'>
+            <h1 className='text-left text-3xl font-bold text-indigo-800'>
+              Programming
+            </h1>
+
+            <li className='text-left text-lg font-bold text-white'>Python3</li>
+            <li className='text-left text-lg font-bold text-white'>C++</li>
+            <li className='text-left text-lg font-bold text-white'>
+              JavaScript
+            </li>
+
+            <li className='text-left text-lg font-bold text-white'>Node.js</li>
+            <li className='text-left text-lg font-bold text-white'>Php</li>
+            <li className='text-left text-lg font-bold text-white'>Bash</li>
+            <li className='text-left text-lg font-bold text-white'>HTML</li>
+            <li className='text-left text-lg font-bold text-white'>CSS</li>
+            <li className='text-left text-lg font-bold text-white'>SQL</li>
+            <li className='text-left text-lg font-bold text-white'>REST API</li>
+          </div>
+          <div>
+            <h1 className='text-left text-3xl font-bold text-indigo-800'>
+              Database
+            </h1>
+
+            <li className='text-left text-lg font-bold text-white'>MySQL</li>
+            <li className='text-left text-lg font-bold text-white'>
+              PastgreSQl
+            </li>
+            <li className='text-left text-lg font-bold text-white'>
+              phpMyAdmin
+            </li>
+            <li className='text-left text-lg font-bold text-white'>SQLite</li>
+            <li className='text-left text-lg font-bold text-white'>MongoDB</li>
+            <li className='text-left text-lg font-bold text-white'>Mongoose</li>
+          </div>
+        </div>
+        <div className='flex flex-row'>
+          <div className='mr-20'>
+            <h1 className='text-left text-3xl font-bold text-indigo-800'>
+              Frameworks/Services
+            </h1>
+
+            <li className='text-left text-lg font-bold text-white'>React.js</li>
+            <li className='text-left text-lg font-bold text-white'>Flask</li>
+            <li className='text-left text-lg font-bold text-white'>Express</li>
+            <li className='text-left text-lg font-bold text-white'>Ejs</li>
+            <li className='text-left text-lg font-bold text-white'>JQuery</li>
+            <li className='text-left text-lg font-bold text-white'>
+              TensorFlow
+            </li>
+            <li className='text-left text-lg font-bold text-white'>
+              Bootstrap
+            </li>
+            <li className='text-left text-lg font-bold text-white'>
+              Tailwind CSS
+            </li>
+            <li className='text-left text-lg font-bold text-white'>
+              GIt/Github
+            </li>
+            <li className='text-left text-lg font-bold text-white'>Heroku</li>
+            <li className='text-left text-lg font-bold text-white'>
+              Jupyter Notebook
+            </li>
+            <li className='text-left text-lg font-bold text-white'>GOOGLE</li>
+          </div>
+          <div>
+            <h1 className='text-left text-3xl font-bold text-indigo-800'>
+              Launguages
+            </h1>
+
+            <li className='text-left text-lg font-bold text-white'>
+              Gujarati: Proficient in Speaking, Reading, and Writing
+            </li>
+            <li className='text-left text-lg font-bold text-white'>
+              Hindi: Proficient in Speaking, Reading, and Writing
+            </li>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
